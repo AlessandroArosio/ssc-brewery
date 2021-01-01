2634c23939de0516cd5072ff78cf044a819fabea
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-01 17:39:25 +0000

    Adding database authentication (h2)

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,35 +1,17 @@
 package guru.sfg.brewery.config;
 
-import guru.sfg.brewery.security.RestHeaderAuthFilter;
-import guru.sfg.brewery.security.RestUrlAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
-        var filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
-
-    public RestUrlAuthFilter restUrlAuthFilter(AuthenticationManager authenticationManager) {
-        var filter = new RestUrlAuthFilter(new AntPathRequestMatcher("/api/**"));
-        filter.setAuthenticationManager(authenticationManager);
-        return filter;
-    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -38,14 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(restHeaderAuthFilter(authenticationManager()),
-                UsernamePasswordAuthenticationFilter.class)
-        .csrf().disable();
-
-        http.addFilterBefore(restUrlAuthFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
-
         http
                 .authorizeRequests(authorize -> authorize
+                        .antMatchers("/h2-console/**").permitAll() // not use in PROD
                         .antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                         .antMatchers("/beers/find", "/beers*").permitAll()
                         .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
@@ -54,41 +31,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().and()
-                .httpBasic();
+                .httpBasic()
+                .and().csrf().disable();
+
+        // h2 console config
+        http.headers().frameOptions().sameOrigin();
     }
 
     // using fluent API
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("spring")
-                .password("{bcrypt}$2a$10$6Oa/ijV2EBAp4k4S3Cdg7.tjBkcEoHr42Nmo9WeN3kFAsW1.O.1I2")
-                .roles("ADMIN")
-                .and()
-                .withUser("user")
-                .password("{sha256}4617111016211f141eb1f5c5eda7909ff38884f395efbacebd0bbe6c68783aa41a5db7f9eff6e6c8")
-                .roles("USER");
-
-        auth.inMemoryAuthentication()
-                .withUser("scott").password("{bcrypt10}$2a$15$aeC3xXE71/oODEbQCJu2ouI4GLvHk1gg94086GSax3/fZR6SDCnRu")
-                .roles("CUSTOMER");
-    }
-
-    //    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("spring")
-//                .password("guru")
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("spring")
+//                .password("{bcrypt}$2a$10$6Oa/ijV2EBAp4k4S3Cdg7.tjBkcEoHr42Nmo9WeN3kFAsW1.O.1I2")
 //                .roles("ADMIN")
-//                .build();
+//                .and()
+//                .withUser("user")
+//                .password("{sha256}4617111016211f141eb1f5c5eda7909ff38884f395efbacebd0bbe6c68783aa41a5db7f9eff6e6c8")
+//                .roles("USER");
 //
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
+//        auth.inMemoryAuthentication()
+//                .withUser("scott").password("{bcrypt10}$2a$15$aeC3xXE71/oODEbQCJu2ouI4GLvHk1gg94086GSax3/fZR6SDCnRu")
+//                .roles("CUSTOMER");
 //    }
 }
---
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailService.java
@@ -0,0 +1,51 @@
+package guru.sfg.brewery.security;
+
+import guru.sfg.brewery.domain.security.Authority;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class JpaUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.debug("Getting user info via JPA");
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("Username: %s - not found", username)));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), user.getEnabled(), user.getAccountNotExpired(), 
+                user.getCredentialsNotExpired(), user.getAccountNotLocked(), 
+                convertToSpringAuthorities(user.getAuthorities()));
+    }
+
+    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
+        if (!CollectionUtils.isEmpty(authorities)) {
+            return authorities.stream()
+                    .map(Authority::getRole)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toSet());
+        } else {
+            return new HashSet<>();
+        }
+    }
+}
